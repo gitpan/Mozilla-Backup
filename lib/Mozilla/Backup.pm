fb019c853901bec26370b9c0f35f1da567884571
Robert Rothenberg <rrwo@cpan.org>
2005-04-10 15:09:01 +0000

    Import of RRWO/Mozilla-Backup-0.03 from CPAN.
    gitpan-cpan-distribution: Mozilla-Backup
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         RRWO/Mozilla-Backup-0.03.tar.gz
    gitpan-cpan-author:       RRWO
    gitpan-cpan-maturity:     released

--- lib/Mozilla/Backup.pm
+++ lib/Mozilla/Backup.pm
@@ -29,7 +29,6 @@ use 5.006;
 use strict;
 use warnings;
 
-use Archive::Zip;
 use Carp;
 use Config::IniFiles;
 use File::Find;
@@ -37,128 +36,258 @@ use File::Spec;
 use Log::Dispatch 1.6;
 use Params::Validate qw( :all );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
-sub _find_mozilla_profiles {
+my @PROFILE_TYPES = qw(
+  camino firefox galeon k-meleon mozilla netscape phoenix sunbird thunderbird
+);
+
+sub profile_types {
+  return @PROFILE_TYPES;
+}
+
+sub _catdir {
+  if ($_[0]) { # otherwise blank "" is translated to root directory
+    my $path = File::Spec->catdir(@_);
+    return (-d $path) ? $path : undef;
+  }
+  else {
+    return;
+  }
+}
+
+sub _catfile {
+  my $path = File::Spec->catfile(@_);
+  return (-r $path) ? $path : undef;
+}
+
+sub _find_profile_path {
+  my $home = shift;
+  my $type = shift;
+
+  my $path;
+
+  # The MOZILLA_HOME environment variables are for OS/2, but putting
+  # them here first allows one to override settings.
+
+  if ($path = _catdir($ENV{uc($type)."_HOME"})) {
+    return $path;
+  }
+  if ($path = _catdir($ENV{MOZILLA_HOME}, ucfirst($type))) {
+    return $path;
+  }
+
+  if ($path = _catdir($home, "\.$type")) {
+    return $path;
+  }
+  if ($path = _catdir($home, "\.mozilla", $type)) {
+    return $path;
+  }
+  if ($path = _catdir($home, ucfirst($type))) {
+    return $path;
+  }
+  if ($path = _catdir($home, "Mozilla", ucfirst($type))) {
+    return $path;
+  }
+
+  if ($^O eq "darwin") {
+    if ($path = _catdir($home, "Library", "Application Support",
+			ucfirst($type))) {
+      return $path;
+    }
+    if ($path = _catdir($home, "Library", "Application Support",
+			"Mozilla", ucfirst($type))) {
+      return $path;
+    }
+    if ($path = _catdir($home, "Library", ucfirst($type))) {
+      return $path;
+    }
+    if ($path = _catdir($home, "Library", "Mozilla", ucfirst($type))) {
+      return $path;
+    }
+  }
+  elsif ($^O eq "MSWin32") {
+    my $program_files = $ENV{ProgramFiles} || "Program Files";
+    if ($path = _catdir($program_files, ucfirst($type))) {
+      return $path;
+    }
+    if ($path = _catdir($program_files, "Mozilla", ucfirst($type))) {
+      return $path;
+    }
+  }
+
+  return;
+}
+
+sub _find_all_profiles {
   my $self = shift;
-  my %apps = ( );
-
-  my %MOZILLA_PROFILE_PATHS = (
-    'mozilla'     => 'Mozilla',
-    'firefox'     => 'Mozilla/Firefox',
-    'sunbird'     => 'Mozilla/Sunbird',
-    'thunderbird' => 'Thunderbird',
-  );
-
-  # Mac OS/X - $ENV{HOME}/Library or $ENV{HOME}/Library/Application Support
-  # Windows 98       - C:\WINDOWS\Application Data
-  # Windows 2000/XP  - $ENV{APPDATA}
-  # Unix     - $ENV{HOME}/.mozilla
-
-  # K-Melion in Windows - $ENV{ProgramFiles}/K-Meleon/Profiles ?
-  # Camino?
 
   my $home = $ENV{HOME};
   if ($^O eq "MSWin32") {
-    $home = $ENV{APPDATA};
-  }
-  else {
-    croak $self->_log( level => "error", message => "unsupported OS: $^O" );
+    $home  = $ENV{APPDATA} || _catdir($ENV{USERPROFILE}, "Application Data");
   }
 
-    if (-d $home) {
+  foreach my $type (profile_types) {
+    if (my $path = _find_profile_path($home,$type)) {      
+      if (my $ini_file = _catfile($path, "profiles.ini")) {
+	$self->{ini_files}->{$type} = $ini_file;
+	my $cfg = Config::IniFiles->new( -file => $ini_file );
+	if ($cfg) {
+	  my $i = 0;
+	  while (my $profile = $cfg->val("Profile$i","Path")) {
 
-      foreach my $app (keys %MOZILLA_PROFILE_PATHS) {
-	my $path = $MOZILLA_PROFILE_PATHS{$app};
-	my $app_path = File::Spec->catdir($home,$path);
+	    my $profile_path = _catdir($path, $profile);
+	    $profile_path = _catdir($profile) unless ($profile_path);
 
-	if (-d $app_path) {
-	  my $ini_file = File::Spec->catfile($app_path, 'profiles.ini');
-	  $self->{ini_files}->{$app} = $ini_file;
-
-	  if (-r $ini_file) {
-	    my $cfg = Config::IniFiles->new( -file => $ini_file );
-	    if ($cfg) {
-	      my $i = 0;
-	      while (my $profile = $cfg->val("Profile$i","Path")) {
-		my $profile_path = File::Spec->catdir($app_path, $profile);
-		if (-d $profile_path) {
-		  my $data = {
-                    Profile => "Profile$i",
-                    Path    => $profile_path,
-                  };
-		  foreach my $key (qw( Name IsRelative )) {
-		    $data->{$key} = $cfg->val("Profile$i",$key);
-		  }
-		  $apps{$app} = [ ] unless (exists $apps{$app});
-		  push @{$apps{$app}}, $data;
-		} else {
-		  $self->_log( level => "warn", message => "$profile_path not a directory");
-		}
-		$i++;
+	    if ($profile_path) {
+	      my $data = {
+		ProfileId => "Profile$i",
+		Path      => $profile_path,
+	      };
+	      foreach my $key (qw( Name IsRelative )) {
+		$data->{$key} = $cfg->val("Profile$i",$key);
 	      }
-	    } else {
-	      croak $self->_log( level => "error", message => "Bad INI file: $ini_file");
-	    }
-	  }
-	}
-      }
-      
-    }
 
-  foreach my $app (keys %apps) {
-    $self->{profiles}->{$app} = { };
-    foreach my $profile (@{$apps{$app}}) {
-      $self->{profiles}->{$app}->{$profile->{Name}} = $profile;
+	      $self->{profiles}->{$type} = { }
+		unless (exists $self->{profiles}->{type});
+	      $self->{profiles}->{$type}->{$data->{Name}} = $data;
+
+	    } else {
+	      croak $self->_log( level => "error",
+		message => "$profile_path not a directory");
+	    }
+	    $i++;
+	  }
+	} else {
+	  croak $self->_log( level => "error",
+	    message => "Bad INI file: $ini_file");
+	}
+
+
+      }
     }
   }
-
 }
 
 =over
 
 =item new
 
-  $moz = Mozilla::Backup->new( log => $log );
+  $moz = Mozilla::Backup->new( %options );
 
-Creates a new Mozilla::Backup object.  C<$log> is an optional
-L<Log::Dispatch> object.
+Creates a new Mozilla::Backup object. The options are as follows:
+
+=over
+
+=item log
+
+A L<Log::Dispatch> object for receiving log messages.
+
+=item new_arch
+
+A callback for creating a new archive, which receives the following
+arguments: C<($moz, $path, %opts)>.
+
+C<$moz> is the L<Mozilla::Backup> object. 
+
+C<$path> is the pathname of the archive.
+
+<%opts> are options specific to the archive format.
+
+You may save state in the C<$moz->{archive}->{user}> key.
+
+On success it returns a true value.
+
+=item add_arch
+
+A callback for adding a file to the archive, which receives the following
+arguments: C<$moz, $local_file, $archive_file>.
+
+=item end_arch
+
+A callback for saving and closing the archive. It receives the following
+arguments: C<$moz>.
+
+=back
 
 =cut
+
+# TODO:
+# - option to insert comments into archive
+# - option to control compression type
 
 sub new {
   my $class = shift || __PACKAGE__;
 
   my %args  = validate( @_, {
-    log       => { default => Log::Dispatch->new, },
+    log       => {
+                   default => Log::Dispatch->new,
+                   isa     => 'Log::Dispatch',
+                 },
+    new_arch  => {
+                   default => \&_new_archive,
+                   type    => 'CODE',
+                 },
+    add_arch  => {
+                   default => \&_add_archive,
+                   type    => 'CODE',
+                 },
+    end_arch  => {
+                   default => \&_end_archive,
+                   type    => 'CODE',
+                 },
   });
 
   my $self  = {
     log       => $args{log},
     profiles  => { },
     ini_files => { },
+    archive   => {
+      _new    => $args{new_arch},
+      _add    => $args{add_arch},
+      _end    => $args{end_arch},
+      path    => undef,
+      opts    => { },
+    },
   };
+
 
   bless $self, $class;
 
-  $self->_find_mozilla_profiles();
+  $self->_find_all_profiles();
 
   return $self;
 }
+
+=item profile_types
+
+  @types = $moz->profile_types;
+
+Returns a list of all profile types that are supported by this version
+of the module.
+
+Supported profile types:
+
+  camino
+  firefox
+  galeon
+  kmeleon
+  mozilla
+  phoenix
+  netscape
+  sunbird
+  thunderbird
+
+Some of these profile types are for platform-specific applications, so
+you may never run into them.
 
 =item found_profile_types
 
   @types = $moz->found_profile_types();
 
 Returns a list of applications for which profiles were found.  (This
-does not mean that the applications are installed on the machine.)
-
-Supported profile types:
-
-  firefox
-  mozilla
-  sunbird
-  thunderbird
+does not mean that the applications are installed on the machine, only
+that profiles were found where they were expected.)
 
 =cut
 
@@ -177,6 +306,7 @@ Returns the profile INI file for that type.
 
 sub ini_file {
   my $self = shift;
+  validate_pos(@_, 1);
   my $type = shift;
   return $self->{ini_files}->{$type};
 }
@@ -191,6 +321,7 @@ Returns the names of profiles associated with the type.
 
 sub profile_names {
   my $self = shift;
+  validate_pos(@_, 1);
   my $type = shift;
   return (keys %{$self->{profiles}->{$type}});
 }
@@ -205,9 +336,27 @@ Returns the pathname of the profile.
 
 sub profile_path {
   my $self = shift;
+  validate_pos(@_, 1, 1);
   my $type = shift;
   my $name = shift;
   return $self->{profiles}->{$type}->{$name}->{Path};
+}
+
+
+=item profile_exists
+
+  if ($moz->profile_exists($type,$name)) { ... }
+
+Returns true if a profile exists.
+
+=cut
+
+sub profile_exists {
+  my $self = shift;
+  validate_pos(@_, 1, 1);
+  my $type = shift;
+  my $name = shift;
+  return (exists $self->{profiles}->{$type}->{$name});
 }
 
 =item profile_is_relative
@@ -220,50 +369,102 @@ Returns the 'IsRelative' flag for the profile.
 
 sub profile_is_relative {
   my $self = shift;
+  validate_pos(@_, 1, 1);
   my $type = shift;
   my $name = shift;
   return $self->{profiles}->{$type}->{$name}->{IsRelative};
 }
 
+=item profile_id
 
-=item profile_section
+  $section = $moz->profile_id($type,$name);
 
-  $section = $moz->profile_section($type,$name);
-
-Returns the L</ini_file> section of the profile.
+Returns the L</ini_file> identifier of the profile.
 
 =cut
 
-sub profile_section {
+sub profile_id {
   my $self = shift;
+  validate_pos(@_, 1, 1);
   my $type = shift;
   my $name = shift;
-  return $self->{profiles}->{$type}->{$name}->{Profile};
+  return $self->{profiles}->{$type}->{$name}->{ProfileId};
+}
+
+
+=item profile_is_locked
+
+  if ($moz->profile_is_locked($type,$name)) { ... }
+
+Returns true if there is a lock file in the profile.
+
+=cut
+
+sub profile_is_locked {
+  my $self = shift;
+  validate_pos(@_, 1, 1);
+  my $type = shift;
+  my $name = shift;
+  return (-e File::Spec->catfile($self->profile_path($type,$name),
+				 'parent.lock'));
+}
+
+sub _new_archive {
+  my $self = shift;
+  my $path = shift;
+  my %opts = @_;  
+
+  $self->{archive}->{path} = $path;
+  $self->{archive}->{opts} = \%opts;
+
+  require Archive::Zip;
+
+  return $self->{archive}->{zip} = Archive::Zip->new();
+}
+
+sub _add_archive {
+  my $self = shift;
+  my $file = shift;                 # actual file
+  my $name = shift || $file;        # name in archive
+
+  $self->{archive}->{zip}->addFileOrDirectory($file, $name);
+}
+
+sub _end_archive {
+  my $self = shift;
+  $self->{archive}->{zip}->writeToFileNamed( $self->{archive}->{path} );
 }
 
 sub _backup_path {
   my $self = shift;
+  validate_pos(@_, 1, 1, 1);
   my $path = shift;
-  my $back = shift;
+  my $dest = shift;
 
-  if (-e $back) {
-    croak $self->_log( level => "error", message => "$back exists already" );
+  my $relative = shift;
+
+  if (-e $dest) {
+    croak $self->_log( level => "error", message => "$dest exists already" );
   }
 
 
   $self->_log( level => "notice", message => "Backing up $path\n" );
-  my $arch = Archive::Zip->new();
+
+  unless ($self->{archive}->{_new}->($self, $dest)) {
+    croak $self->_log( level => "error", message => "error creating archive" );
+  }
+
   find({
 	bydepth    => 1,
 	wanted     => sub {
 	  my $file = $File::Find::name;
-	  my $name = substr($file, length($path));
+	  my $name = $relative ? substr($file, length($path)) : $file;
 	  if ($name) {
 	    $name = substr($name,1); # remove initial '/'
 	    unless ($name =~ /^(Cache(\.Trash)?|extensions)\//) {
 	      $name .= '/' if (-d $file);
               $self->_log( level => "info", message => "Backing up $name\n" );
-	      $arch->addFileOrDirectory($file, $name);
+              $self->{archive}->{_add}->($self, $file, $name);
 	    }
 	  }
 
@@ -272,13 +473,13 @@ sub _backup_path {
       );
 
   # TODO: check for errors here
-  $self->_log( level => "notice", message => "Saving to $back\n" );
-  $arch->writeToFileNamed( $back );
+  $self->_log( level => "notice", message => "Saving to $dest\n" );
+  $self->{archive}->{_end}->($self);
 }
 
 =item backup_profile
 
-  $file = $moz->backup_profile($type,$name,$dest,$arch);
+  $file = $moz->backup_profile($type,$name,$dest,$arch,$rel);
 
 Backs up the profile as a zip archive to the path specified in C<$dest>.
 (If none is given, the current directory is assumed.)
@@ -286,23 +487,40 @@ Backs up the profile as a zip archive to the path specified in C<$dest>.
 C<$arch> is an optional name for the archive file. If none is given, it
 assumes F<type-name-date-time.zip>.
 
+C<$rel> is an optional flag to backup files with relative paths instead
+of absolute pathnames. It defaults to the value of L</profile_is_relative>
+for that profile. (Non-relative profiles will show a warning message.)
+
 If the profile is currently in use, it may not be backed up properly.
+
+This version does no munging of the profile data at all. It simply
+stores the files in an archive.
 
 =cut
 
 sub backup_profile {
   my $self = shift;
+  validate_pos(@_, 1, 1, 0, 0, 0);
   my $type = shift;
   my $name = shift;
   my $dest = shift || '.';
   my $arch = shift || $self->_archive_name($type, $name);
   my $back = File::Spec->catfile($dest, $arch);
 
-  unless ($self->profile_is_relative($type,$name)) {
-    $self->_log( level => "warn", message => "warning: profile is not relative" );
+  my $relative = shift;
+
+  $relative = $self->profile_is_relative($type,$name)
+    unless (defined $relative);
+
+  unless ($relative) {
+    $self->_log( level => "notice", message => "backup will not use relative pathnames" );
   }
 
-  $self->_backup_path($self->profile_path($type,$name), $back);
+  if ($self->profile_is_locked($type,$name)) {
+    croak $self->_log( level => "error", message => "cannot backup locked profile" );
+  }
+
+  $self->_backup_path($self->profile_path($type,$name), $back, $relative);
   return $back;
 }
 
@@ -344,7 +562,9 @@ sub restore_profile {
 
 This module is a prototype. Use at your own risk!
 
-Only Windows 2000/XP machines are supported in this version.
+Not all of the profile types have been tested, and are implemented
+based on information gleaned from sources which may or may not be
+accurate.
 
 =head1 SEE ALSO
 
--- t/Mozilla-Backup.t
+++ t/Mozilla-Backup.t
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use Test::More tests => 35;
+use Test::More tests => 94;
 
 use File::Temp qw( tempdir );
 
@@ -12,6 +12,9 @@ ok(defined $moz, "new");
 
 my $tmpdir = tempdir( CLEANUP => 1 );
 
+my @all   = $moz->profile_types;
+ok(@all, "profile_types");
+
 my %types = map { $_ => 1 } ($moz->found_profile_types);
 
 SKIP: {
@@ -19,10 +22,10 @@ SKIP: {
   ok(%types, "found_profile_types");
 }
 
-foreach my $type (qw( mozilla firefox thunderbird sunbird )) {
+foreach my $type (@all) {
 
  SKIP: {
-    skip "No $type profiles found", 8 unless ($types{$type});
+    skip "No $type profiles found", 10 unless ($types{$type});
 
     my @profs = $moz->profile_names($type);
     ok(@profs, "$type profile_names");
@@ -30,25 +33,34 @@ foreach my $type (qw( mozilla firefox thunderbird sunbird )) {
     ok(-r $moz->ini_file($type), "$type ini_file");
 
     my $name  = $profs[0];
+    ok($moz->profile_exists($type,$name), "profile exists");
+
     ok(-d $moz->profile_path($type,$name), "$type $name profile_path");
 
-    {
-      local $TODO = "should be relative";
-      ok($moz->profile_is_relative($type,$name), "profile_is_relative");
-    }
+    ok(defined $moz->profile_is_relative($type,$name), "profile_is_relative");
 
-    ok($moz->profile_section($type,$name), "profile_section");
+    ok($moz->profile_id($type,$name), "profile_id");
 
     my $arch = $moz->_archive_name($type,$name);
     ok($arch =~ /^$type\-$name\-\d{8}\-\d{6}\.zip$/, "archive_name");
 
+    skip "profile is locked", 3 if ($moz->profile_is_locked($type,$name));
+
     my $file = $moz->backup_profile($type,$name,$tmpdir,$arch);
     ok(-e $file, "backup_profile");
+
+    my $verify = File::Spec->catfile($tmpdir,$arch);
+    ok(-e $verify, "file is where expected to be");
+
+    # TODO - test that it is a valid zip file,
+    #      - test that it does not have files that were excluded
+    #        (such as Cache, etc.)
+    #      - test that it is relative vs. absolute as appropriate
+    #      - test separate backup that is opposite of profile_is_relative
 
     eval {
       $moz->restore_profile($type,"bogus_profile_name");
     };
     ok($@, "restore_profile fails");
-
   }
 }
